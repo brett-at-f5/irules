7e7f1633fbfe8540011c4dd3d23daf1a23ea05a2
brett-at-f5 <34260277+brett-at-f5@users.noreply.github.com>
2018-11-21 10:24:01 +1000

    Extract the User Principal Name (UPN) from a x.509
    This iRule will Extract the User Principal Name (UPN) from a x.509 client certificate extension

--- extract_upn_apm_event_irule.tcl
+++ extract_upn_apm_event_irule.tcl
@@ -1,6 +1,8 @@
-#** Author: brett-at-f5
 #**
-#** This iRule will Extract the User Principal Name (UPN) from a x.509 client certificate extenstion.
+#** Name: extract_upn_apm_event_irule
+#** Author: brett-at-f5
+#** Description: This iRule will Extract the User Principal Name (UPN) from a x.509 client certificate extension.
+#**
 
 when ACCESS_POLICY_AGENT_EVENT {
   if { [ACCESS::policy agent_id] eq "extract_upn" } {
