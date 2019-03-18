483c0d8d4c562032d114fb3f41ee0cc8d21c6801
brett-at-f5 <34260277+brett-at-f5@users.noreply.github.com>
2019-03-18 21:16:41 +1000

    Update dns_safe_search_irule.tcl

--- dns_safe_search_irule.tcl
+++ dns_safe_search_irule.tcl
@@ -5,35 +5,48 @@
 #**
 
 when RULE_INIT {
+  # SafeSearch Bypass List (IPv4)
+  set static::dns_safe_search_bypass_dg "dns_safe_search_bypass_dg"
+
   # SafeSearch CNAME Record mapping
-	set static::dns_safe_search_cname_dg "dns_safe_search_cname_dg"
+  set static::dns_safe_search_cname_dg "dns_safe_search_cname_dg"
 
   # DNS Resolver
-	set static::dns_resolver "/Common/ns1.f5.demo_dns_udp_vs"
+  set static::dns_resolver "/Common/dns_udp_vs"
 
-	# TTL value for CNAME Record
-	set static::dns_ttl 30
-}
-
+  # TTL value for CNAME Record
+  set static::dns_ttl 30
 
 when DNS_REQUEST priority 100 {
-  # If the FQDN in the data group matches the Question Name, send a DNS Response with the CNAME in data group
-  if { (([class match [DNS::question name] equals $static::dns_safe_search_cname_dg]) and ([DNS::question type] eq "A")) } {
-       
-    set cname [class match -value [DNS::question name] equals $static::dns_safe_search_cname_dg]
-    set ipv4  [lindex [RESOLV::lookup @$static::dns_resolver $cname] 0]
+  # If the Client IP is not in the Bypass List, filter enable SafeSearch.
+  if { !([class match [IP::client_addr] equals $static::dns_safe_search_bypass_dg]) } {
+    # If the FQDN in the data group matches the Question Name, send a DNS Response with the SafeSeach CNAME in data group
+    if { (([class match [DNS::question name] equals $static::dns_safe_search_cname_dg]) and ([DNS::question type] eq "A")) } {
+ 
+      set cname [class match -value [DNS::question name] equals $static::dns_safe_search_cname_dg]
+      set ipv4  [lindex [RESOLV::lookup @$static::dns_resolver $cname] 0]
 
-    DNS::header rcode NOERROR
-    DNS::answer insert "[DNS::question name]. $static::dns_ttl [DNS::question class] CNAME $cname"
-    DNS::answer insert "$cname. $static::dns_ttl [DNS::question class] A $ipv4"
-    DNS::header ra "1"
-    DNS::return 
+      # Send the SafeSeach DNS Response
+      DNS::header rcode NOERROR
+      DNS::answer insert "[DNS::question name]. $static::dns_ttl [DNS::question class] CNAME $cname"
+      DNS::answer insert "$cname. $static::dns_ttl [DNS::question class] A $ipv4"
+      DNS::header ra "1"
+      DNS::return
+    }
   }
 }
 
-#**
-#** dns_safe_search_cname_dg
-#**
+##** dns_safe_search_bypass_dg Example
+
+ltm data-group internal dns_safe_search_bypass_dg {
+    records {
+        10.10.10.31/32 { }
+        10.10.99.31/32 { }
+    }
+    type ip
+}
+
+##** dns_safe_search_cname_dg Example
 
 ltm data-group internal dns_safe_search_cname_dg {
     records {
