f6ab1e62be5fbc0e4a0835bbb0a56dd2d83182a0
brett-at-f5 <34260277+brett-at-f5@users.noreply.github.com>
2019-06-11 11:59:41 +1000

    Update log_dns_irule.tcl

--- log_dns_irule.tcl
+++ log_dns_irule.tcl
@@ -7,7 +7,7 @@
 when RULE_INIT {
   # Debug logging control.
   # 0 = no logging, 1 = debug logging (Test/Dev Only).
-  set static::log_dns_dbg 0
+  set static::log_dns_dbg 1
 }
 
 proc logger { log_message } {
@@ -16,8 +16,8 @@ proc logger { log_message } {
   }
 }
 
-when DNS_REQUEST {
-  set dns_request_msg "ptype=[DNS::ptype],id=[DNS::header id],name=[DNS::question name],class=[DNS::question class],dns_type=[DNS::question type],"
+when DNS_REQUEST priority 100 {
+  set dns_request_msg "ptype=[DNS::ptype],opcode=[DNS::header opcode],id=[DNS::header id],name=[DNS::question name],class=[DNS::question class],dns_type=[DNS::question type],"
   append dns_request_msg "source_ip=[IP::client_addr],timestamp=[clock seconds]"
 
   # Log DNS Request details
@@ -27,17 +27,15 @@ when DNS_REQUEST {
 when DNS_RESPONSE {
   if { [DNS::ptype] eq "ANSWER" } {
     foreach rr [DNS::answer] {
-      set dns_response_msg "ptype=[DNS::ptype],id=[DNS::header id],name=[DNS::name $rr],class=[DNS::class $rr],dns_type=[DNS::type $rr],ttl=[DNS::ttl $rr],"
+      set dns_response_msg "ptype=[DNS::ptype],rcode=[DNS::header rcode],id=[DNS::header id],name=[DNS::name $rr],class=[DNS::class $rr],dns_type=[DNS::type $rr],ttl=[DNS::ttl $rr],"
       append dns_response_msg "rdata=[DNS::rdata $rr],source_ip=[IP::client_addr],server_ip=[LB::server addr],timestamp=[clock seconds]"
 
       # Log DNS Response via HSL
       call logger $dns_response_msg
     }
-  }
-
-  if { [DNS::ptype] eq "NXDOMAIN" } {
-    set dns_response_msg "ptype=[DNS::ptype],id=[DNS::header id],name=[DNS::question name],class=[DNS::question class],dns_type=[DNS::question type],"
-    append dns_response_msg "source_ip=[IP::client_addr],server_ip=[LB::server addr],timestamp=[clock seconds]"
+  } else {
+    set dns_response_msg "ptype=[DNS::ptype],rcode=[DNS::header rcode],id=[DNS::header id],name=[DNS::question name],class=[DNS::question class],dns_type=[DNS::question type],ttl=,"
+    append dns_response_msg "rdata=,source_ip=[IP::client_addr],server_ip=[LB::server addr],timestamp=[clock seconds]"
 
     # Log DNS Response details
     call logger $dns_response_msg
