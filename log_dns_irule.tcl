0e723e818fbcfb815b38ca3a24b9221bf3b5b8ee
brett-at-f5 <34260277+brett-at-f5@users.noreply.github.com>
2019-07-22 15:10:32 +1000

    Update log_dns_irule.tcl

--- log_dns_irule.tcl
+++ log_dns_irule.tcl
@@ -17,7 +17,7 @@ proc logger { log_message } {
 }
 
 when DNS_REQUEST priority 100 {
-  set dns_request_msg "ptype=[DNS::ptype],opcode=[DNS::header opcode],id=[DNS::header id],name=[DNS::question name],class=[DNS::question class],dns_type=[DNS::question type],"
+  set dns_request_msg "ptype=[DNS::ptype],origin=[DNS::origin],opcode=[DNS::header opcode],id=[DNS::header id],name=[DNS::question name],class=[DNS::question class],dns_type=[DNS::question type],"
   append dns_request_msg "source_ip=[IP::client_addr],timestamp=[clock seconds]"
 
   # Log DNS Request details
@@ -27,14 +27,14 @@ when DNS_REQUEST priority 100 {
 when DNS_RESPONSE {
   if { [DNS::ptype] eq "ANSWER" } {
     foreach rr [DNS::answer] {
-      set dns_response_msg "ptype=[DNS::ptype],rcode=[DNS::header rcode],id=[DNS::header id],name=[DNS::name $rr],class=[DNS::class $rr],dns_type=[DNS::type $rr],ttl=[DNS::ttl $rr],"
+      set dns_response_msg "ptype=[DNS::ptype],origin=[DNS::origin],rcode=[DNS::header rcode],id=[DNS::header id],name=[DNS::name $rr],class=[DNS::class $rr],dns_type=[DNS::type $rr],ttl=[DNS::ttl $rr],"
       append dns_response_msg "rdata=[DNS::rdata $rr],source_ip=[IP::client_addr],server_ip=[LB::server addr],timestamp=[clock seconds]"
 
       # Log DNS Response via HSL
       call logger $dns_response_msg
     }
   } else {
-    set dns_response_msg "ptype=[DNS::ptype],rcode=[DNS::header rcode],id=[DNS::header id],name=[DNS::question name],class=[DNS::question class],dns_type=[DNS::question type],ttl=,"
+    set dns_response_msg "ptype=[DNS::ptype],origin=[DNS::origin],rcode=[DNS::header rcode],id=[DNS::header id],name=[DNS::question name],class=[DNS::question class],dns_type=[DNS::question type],ttl=,"
     append dns_response_msg "rdata=,source_ip=[IP::client_addr],server_ip=[LB::server addr],timestamp=[clock seconds]"
 
     # Log DNS Response details
