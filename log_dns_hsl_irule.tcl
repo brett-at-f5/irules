29d56ca304fe171ae3bca18229cbee9e4961bb50
brett-at-f5 <34260277+brett-at-f5@users.noreply.github.com>
2019-07-25 14:45:44 +1000

    Update log_dns_hsl_irule.tcl

--- log_dns_hsl_irule.tcl
+++ log_dns_hsl_irule.tcl
@@ -8,7 +8,7 @@ when DNS_REQUEST {
   set hsl [HSL::open -publisher /Common/elk_log_pub]
 
   # Log DNS Request details
-  HSL::send $hsl "[virtual],timestamp=[clock clicks -milliseconds],ptype=[DNS::ptype],origin=[DNS::origin],opcode=[DNS::header opcode],id=[DNS::header id],name=[DNS::question name],class=[DNS::question class],dns_type=[DNS::question type],source_ip=[IP::client_addr]"
+  HSL::send $hsl "timestamp=[clock clicks -milliseconds],vs=[virtual],ptype=[DNS::ptype],origin=[DNS::origin],opcode=[DNS::header opcode],id=[DNS::header id],name=[DNS::question name],class=[DNS::question class],dns_type=[DNS::question type],source_ip=[IP::client_addr]"
 }
 
 when DNS_RESPONSE {
@@ -17,12 +17,12 @@ when DNS_RESPONSE {
   if { [DNS::ptype] eq "ANSWER" } {
     foreach rr [DNS::answer] {
       # Log DNS Response via HSL
-      HSL::send $hsl "[virtual],timestamp=[clock clicks -milliseconds],ptype=[DNS::ptype],origin=[DNS::origin],rcode=[DNS::header rcode],id=[DNS::header id],name=[DNS::name $rr],class=[DNS::class $rr],dns_type=[DNS::type $rr],ttl=[DNS::ttl $rr],rdata=[DNS::rdata $rr],source_ip=[IP::client_addr],dest_ip=[LB::server addr]"
+      HSL::send $hsl "timestamp=[clock clicks -milliseconds],vs=[virtual],ptype=[DNS::ptype],origin=[DNS::origin],rcode=[DNS::header rcode],id=[DNS::header id],name=[DNS::name $rr],class=[DNS::class $rr],dns_type=[DNS::type $rr],ttl=[DNS::ttl $rr],rdata=[DNS::rdata $rr],source_ip=[IP::client_addr],dest_ip=[LB::server addr]"
     }
   }
 
   if { [DNS::ptype] eq "NXDOMAIN" } {
     # Log DNS Response via HSL
-    HSL::send $hsl "[virtual],timestamp=[clock clicks -milliseconds],ptype=[DNS::ptype],origin=[DNS::origin],rcode=[DNS::header rcode],id=[DNS::header id],name=[DNS::question name],class=[DNS::question class],dns_type=[DNS::question type],source_ip=[IP::client_addr],dest_ip=[LB::server addr]"
+    HSL::send $hsl "timestamp=[clock clicks -milliseconds],vs=[virtual],ptype=[DNS::ptype],origin=[DNS::origin],rcode=[DNS::header rcode],id=[DNS::header id],name=[DNS::question name],class=[DNS::question class],dns_type=[DNS::question type],source_ip=[IP::client_addr],dest_ip=[LB::server addr]"
   }
 }
