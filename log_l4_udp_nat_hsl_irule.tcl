35d47e7a788f711e582a704a158be04cd9bedb6f
brett-at-f5 <34260277+brett-at-f5@users.noreply.github.com>
2019-07-25 17:41:36 +1000

    Update log_l4_udp_nat_hsl_irule.tcl

--- log_l4_udp_nat_hsl_irule.tcl
+++ log_l4_udp_nat_hsl_irule.tcl
@@ -7,5 +7,5 @@
 when SERVER_CONNECTED {
   set hsl [HSL::open -publisher /Common/elk_log_pub]
   # Log NAT connection
-  HSL::send $hsl "timestamp=[clock clicks -milliseconds],vs=[virtual],event=SERVER_CONNECTED,ip_protocol=[IP::protocol],cs_source_ip=[IP::client_addr],cs_source_port=[UDP::client_port],cs_dest_ip=[IP::server_addr],cs_dest_port=[UDP::server_port],ss_source_ip=[IP::local_addr],ss_source_port=[UDP::local_port],ss_dest_ip=[LB::server addr],ss_dest_port=[LB::server port]"
+  HSL::send $hsl "timestamp=[clock clicks -milliseconds],vs=[virtual],event=SERVER_CONNECTED,ip_protocol=[IP::protocol],cs_source_ip=[IP::client_addr],cs_source_port=[UDP::client_port],cs_dest_ip=[clientside {IP::local_addr}],cs_dest_port=[clientside {UDP::local_port}],ss_source_ip=[IP::local_addr],ss_source_port=[UDP::local_port],ss_dest_ip=[LB::server addr],ss_dest_port=[LB::server port]"
 }
