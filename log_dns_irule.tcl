56997f7331618ea2c5507e1c58551b0032e2cccf
brett-at-f5 <34260277+brett-at-f5@users.noreply.github.com>
2019-07-25 18:44:14 +1000

    Update log_dns_irule.tcl

--- log_dns_irule.tcl
+++ log_dns_irule.tcl
@@ -18,17 +18,17 @@ proc log_dns_dbg { log_message } {
 
 when DNS_REQUEST priority 50 {
   # Log DNS Request
-  call log_dns_dbg "[virtual],timestamp=[clock clicks -milliseconds],ptype=[DNS::ptype],origin=[DNS::origin],opcode=[DNS::header opcode],id=[DNS::header id],name=[DNS::question name],class=[DNS::question class],dns_type=[DNS::question type],source_ip=[IP::client_addr],dest_ip=[IP::local_addr],server_ip=[LB::server addr]"
+  call log_dns_dbg "timestamp=[clock clicks -milliseconds],[virtual],ptype=[DNS::ptype],origin=[DNS::origin],opcode=[DNS::header opcode],id=[DNS::header id],name=[DNS::question name],class=[DNS::question class],dns_type=[DNS::question type],source_ip=[IP::client_addr],dest_ip=[IP::local_addr]"
 }
 
 when DNS_RESPONSE {
   if { [DNS::ptype] eq "ANSWER" } {
     foreach rr [DNS::answer] {
       # Log DNS Response
-      call log_dns_dbg "[virtual],timestamp=[clock clicks -milliseconds],ptype=[DNS::ptype],origin=[DNS::origin],rcode=[DNS::header rcode],id=[DNS::header id],name=[DNS::name $rr],class=[DNS::class $rr],dns_type=[DNS::type $rr],ttl=[DNS::ttl $rr],rdata=[DNS::rdata $rr],source_ip=[IP::client_addr],dest_ip=[IP::local_addr],server_ip=[LB::server addr]"
+      call log_dns_dbg "timestamp=[clock clicks -milliseconds],[virtual],ptype=[DNS::ptype],origin=[DNS::origin],rcode=[DNS::header rcode],id=[DNS::header id],name=[DNS::name $rr],class=[DNS::class $rr],dns_type=[DNS::type $rr],ttl=[DNS::ttl $rr],rdata=[DNS::rdata $rr],source_ip=[IP::client_addr],dest_ip=[IP::local_addr],server_ip=[LB::server addr]"
     }
   } else {
     # Log DNS Response
-    call log_dns_dbg "[virtual],timestamp=[clock clicks -milliseconds],ptype=[DNS::ptype],origin=[DNS::origin],rcode=[DNS::header rcode],id=[DNS::header id],name=[DNS::question name],class=[DNS::question class],dns_type=[DNS::question type],ttl=,rdata=,source_ip=[IP::client_addr],dest_ip=[IP::local_addr],server_ip=[LB::server addr]"
+    call log_dns_dbg "timestamp=[clock clicks -milliseconds],[virtual],ptype=[DNS::ptype],origin=[DNS::origin],rcode=[DNS::header rcode],id=[DNS::header id],name=[DNS::question name],class=[DNS::question class],dns_type=[DNS::question type],ttl=,rdata=,source_ip=[IP::client_addr],dest_ip=[IP::local_addr],server_ip=[LB::server addr]"
   }
 }
