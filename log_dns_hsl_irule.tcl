e8a61698ab785346156ad7e4699591f1ea16ffe3
brett-at-f5 <34260277+brett-at-f5@users.noreply.github.com>
2019-10-08 21:05:20 +1000

    Update log_dns_hsl_irule.tcl

--- log_dns_hsl_irule.tcl
+++ log_dns_hsl_irule.tcl
@@ -5,29 +5,22 @@
 #**
 
 when DNS_REQUEST priority 100 {
-  set hsl [HSL::open -publisher /Common/elk_log_pub]
+  set hsl [HSL::open -publisher /Common/elk_log_5402_pub]
 
   # Log DNS Request details
-  HSL::send $hsl "timestamp=[clock clicks -milliseconds],vs=[virtual],ptype=[DNS::ptype],origin=[DNS::origin],opcode=[DNS::header opcode],id=[DNS::header id],name=[DNS::question name],class=[DNS::question class],dns_type=[DNS::question type],cs_source_ip=[IP::client_addr],cs_source_port=[UDP::client_port],cs_dest_ip=[clientside {IP::local_addr}],cs_dest_port=[clientside {UDP::local_port}]"
+  HSL::send $hsl "timestamp=[clock clicks -milliseconds],vs=\"[virtual]\",ptype=\"[DNS::ptype]\",origin=\"[DNS::origin]\",opcode=\"[DNS::header opcode]\",id=[DNS::header id],name=\"[DNS::question name]\",class=\"[DNS::question class]\",dns_type=\"[DNS::question type]\",category=\"\",cs_source_ip=[IP::client_addr],cs_dest_ip=[clientside {IP::local_addr}]"
 }
 
 when DNS_RESPONSE {
-  set hsl [HSL::open -publisher /Common/elk_log_pub]
+  set hsl [HSL::open -publisher /Common/elk_log_5402_pub]
 
   if { [DNS::ptype] eq "ANSWER" } {
     foreach rr [DNS::answer] {
       # Log DNS Response via HSL
-      if { ([LB::server addr] eq "") } {
-        set ss_dest_ip ""
-        set ss_dest_port ""
-      } else {
-      	set ss_dest_ip [LB::server addr]
-      	set ss_dest_port [LB::server port]
-      }
-      HSL::send $hsl "timestamp=[clock clicks -milliseconds],vs=[virtual],ptype=[DNS::ptype],origin=[DNS::origin],rcode=[DNS::header rcode],id=[DNS::header id],name=[DNS::name $rr],class=[DNS::class $rr],dns_type=[DNS::type $rr],ttl=[DNS::ttl $rr],rdata=[DNS::rdata $rr],ss_dest_ip=$ss_dest_ip,ss_dest_port=$ss_dest_port"
+      HSL::send $hsl "timestamp=[clock clicks -milliseconds],vs=\"[virtual]\",ptype=\"[DNS::ptype]\",origin=\"[DNS::origin]\",rcode=\"[DNS::header rcode]\",id=[DNS::header id],name=\"[DNS::name $rr]\",class=\"[DNS::class $rr]\",dns_type=\"[DNS::type $rr]\",ttl=[DNS::ttl $rr],rdata=\"[DNS::rdata $rr]\",category=\"\",cs_source_ip=[IP::client_addr],cs_dest_ip=[clientside {IP::local_addr}],ss_source_ip=[serverside {IP::local_addr}],ss_dest_ip=[LB::server addr]"
     }
   } else {
     # Log DNS Response via HSL
-    HSL::send $hsl "timestamp=[clock clicks -milliseconds],vs=[virtual],ptype=[DNS::ptype],origin=[DNS::origin],rcode=[DNS::header rcode],id=[DNS::header id],name=[DNS::question name],class=[DNS::question class],dns_type=[DNS::question type],ttl=,rdata=,ss_dest_ip=[LB::server addr],ss_dest_port=[LB::server port]"
+    HSL::send $hsl "timestamp=[clock clicks -milliseconds],vs=\"[virtual]\",ptype=\"[DNS::ptype]\",origin=\"[DNS::origin]\",rcode=\"[DNS::header rcode]\",id=[DNS::header id],name=\"[DNS::question name]\",class=\"[DNS::question class]\",dns_type=\"[DNS::question type]\",cs_source_ip=[IP::client_addr],cs_dest_ip=[clientside {IP::local_addr}],ss_source_ip=[serverside {IP::local_addr}],ss_dest_ip=[LB::server addr]"
   }
 }
