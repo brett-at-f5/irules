5faa36e0036644b6ae76fa7bd3f0871dfc24176a
brett-at-f5 <34260277+brett-at-f5@users.noreply.github.com>
2019-10-17 15:14:59 +1000

    Update log_dns_category_hsl_irule.tcl

--- log_dns_category_hsl_irule.tcl
+++ log_dns_category_hsl_irule.tcl
@@ -1,14 +1,15 @@
 #**
 #** Name: log_dns_category_hsl_irule
 #** Author: brett-at-f5
-#** Description: Logs DNS Request and Responses to HSL publisher including URL Category and IPI Category
+#** Description: Logs DNS Request and Responses to HSL publisher with URL and IPI Category
 #**
 
 when DNS_REQUEST priority 100 {
+  set hsl [HSL::open -publisher /Common/elk_log_5402_pub]
+
   set category ""
 
-  set hsl [HSL::open -publisher /Common/elk_log_pub]
-  # If the Question Type matches, filter the request.
+  # If the Question Type is A or AAAA, then log the URL Category 
   if { ([DNS::question type] eq "A" || [DNS::question type] eq "AAAA") } {
     # Determine the URL Category for the Question Name.
     set category [lindex [CATEGORY::lookup http://[DNS::question name]] 0]
@@ -16,32 +17,36 @@ when DNS_REQUEST priority 100 {
   }
 
   # Log DNS Request details
-  HSL::send $hsl "timestamp=[clock clicks -milliseconds],vs=[virtual],ptype=[DNS::ptype],origin=[DNS::origin],opcode=[DNS::header opcode],id=[DNS::header id],name=[DNS::question name],category=$category,class=[DNS::question class],dns_type=[DNS::question type],cs_source_ip=[IP::client_addr],cs_source_port=[UDP::client_port],cs_dest_ip=[IP::local_addr],cs_dest_port=[UDP::local_port]"
+  HSL::send $hsl "timestamp=[clock clicks -milliseconds],vs=\"[virtual]\",ptype=\"[DNS::ptype]\",origin=\"[DNS::origin]\",opcode=\"[DNS::header opcode]\",id=[DNS::header id],name=\"[DNS::question name]\",class=\"[DNS::question class]\",dns_type=\"[DNS::question type]\",category=\"$category\",cs_source_ip=[IP::client_addr],cs_dest_ip=[clientside {IP::local_addr}]"
 }
 
 when DNS_RESPONSE {
-  set categories ""
-  set hsl [HSL::open -publisher /Common/elk_log_pub]
+  set hsl [HSL::open -publisher /Common/elk_log_5402_pub]
+
+  set category ""
+  set ss_source_ip ""
+  set ss_dest_ip ""
 
   if { [DNS::ptype] eq "ANSWER" } {
     foreach rr [DNS::answer] {
-      # Log DNS Response via HSL
-      if { [DNS::type $rr] eq "A" } {
-            set category [lindex [IP::reputation [DNS::rdata $rr]] 0]
+      if { [DNS::origin] eq "SERVER" } {
+        set ss_source_ip [serverside {IP::local_addr}]
+        set ss_dest_ip [LB::server addr]
       }
 
-      if { ([LB::server addr] eq "") } {
-        set ss_dest_ip ""
-        set ss_dest_port ""
-      } else {
-      	set ss_dest_ip [LB::server addr]
-      	set ss_dest_port [LB::server port]
+      if { [DNS::type $rr] eq "A" } {
+        set category [IP::reputation [DNS::rdata $rr]]
       }
+
       # Log DNS Response via HSL
-      HSL::send $hsl "timestamp=[clock clicks -milliseconds],vs=[virtual],ptype=[DNS::ptype],origin=[DNS::origin],rcode=[DNS::header rcode],id=[DNS::header id],name=[DNS::name $rr],category=$category,class=[DNS::class $rr],dns_type=[DNS::type $rr],ttl=[DNS::ttl $rr],rdata=[DNS::rdata $rr],ss_dest_ip=$ss_dest_ip,ss_dest_port=$ss_dest_port"
+      HSL::send $hsl "timestamp=[clock clicks -milliseconds],vs=\"[virtual]\",ptype=\"[DNS::ptype]\",origin=\"[DNS::origin]\",rcode=\"[DNS::header rcode]\",id=[DNS::header id],name=\"[DNS::name $rr]\",class=\"[DNS::class $rr]\",dns_type=\"[DNS::type $rr]\",ttl=[DNS::ttl $rr],rdata=\"[DNS::rdata $rr]\",category=\"$category\",cs_source_ip=[IP::client_addr],cs_dest_ip=[clientside {IP::local_addr}],ss_source_ip=$ss_source_ip,ss_dest_ip=$ss_dest_ip"
     }
   } else {
+    if { [DNS::origin] eq "SERVER" } {
+      set ss_source_ip [serverside {IP::local_addr}]
+      set ss_dest_ip [LB::server addr]
+    }
     # Log DNS Response via HSL
-    HSL::send $hsl "timestamp=[clock clicks -milliseconds],vs=[virtual],ptype=[DNS::ptype],origin=[DNS::origin],rcode=[DNS::header rcode],id=[DNS::header id],name=[DNS::question name],category=,class=[DNS::question class],dns_type=[DNS::question type],ttl=,rdata=,ss_dest_ip=[LB::server addr],ss_dest_port=[LB::server port]"
+    HSL::send $hsl "timestamp=[clock clicks -milliseconds],vs=\"[virtual]\",ptype=\"[DNS::ptype]\",origin=\"[DNS::origin]\",rcode=\"[DNS::header rcode]\",id=[DNS::header id],name=\"[DNS::question name]\",class=\"[DNS::question class]\",dns_type=\"[DNS::question type]\",cs_source_ip=[IP::client_addr],cs_dest_ip=[clientside {IP::local_addr}],ss_source_ip=$ss_source_ip,ss_dest_ip=$ss_dest_ip"
   }
 }
