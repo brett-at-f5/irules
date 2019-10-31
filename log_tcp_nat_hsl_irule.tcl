d0353ea22ac7784e752db7c2091e14e4d907845c
brett-at-f5 <34260277+brett-at-f5@users.noreply.github.com>
2019-10-31 13:38:58 +1000

    Update and rename log_l4_tcp_nat_hsl_irule.tcl to log_tcp_nat_hsl_irule.tcl

--- log_tcp_nat_hsl_irule.tcl
+++ log_tcp_nat_hsl_irule.tcl
@@ -1,11 +1,11 @@
 #**
-#** Name   : log_l4_tcp_nat_hsl_irule
+#** Name   : log_tcp_nat_hsl_irule
 #** Author : brett-at-f5
-#** Description: Log L4 TCP NAT connections to HSL Publisher.
+#** Description: Log TCP NAT connections to HSL Publisher.
 #**
 
 when SERVER_CONNECTED {
-  set hsl [HSL::open -publisher /Common/elk_log_pub]
+  set hsl [HSL::open -publisher /Common/hsl_log_5400_pub]
   # Log NAT connection
   HSL::send $hsl "timestamp=[clock clicks -milliseconds],vs=[virtual],event=SERVER_CONNECTED,ip_protocol=[IP::protocol],cs_source_ip=[IP::client_addr],cs_source_port=[TCP::client_port],cs_dest_ip=[clientside {IP::local_addr}],cs_dest_port=[clientside {TCP::local_port}],ss_source_ip=[IP::local_addr],ss_source_port=[TCP::local_port],ss_dest_ip=[LB::server addr],ss_dest_port=[LB::server port]"
 }
