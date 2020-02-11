c73d179431e6337a890a1235c1c42116214c9550
brett-at-f5 <34260277+brett-at-f5@users.noreply.github.com>
2020-02-11 20:31:40 +1000

    Update and rename log_ip_nat_hsl_irule.tcl to logging/log_ip_nat_hsl_irule.tcl

---
+++ logging/log_ip_nat_hsl_irule.tcl
@@ -0,0 +1,12 @@
+#**
+#** Name   : log_ip_nat_hsl_irule
+#** Author : brett-at-f5
+#** Version: 1.0
+#** Description: Log IP NAT connections to HSL Publisher.
+#**
+
+when SERVER_CONNECTED {
+  set hsl [HSL::open -publisher /Common/hsl_log_5400_pub]
+  # Log NAT connection
+  HSL::send $hsl "timestamp=[clock clicks -milliseconds],vs=[virtual],event=SERVER_CONNECTED,ip_protocol=[IP::protocol],cs_source_ip=[IP::client_addr],cs_dest_ip=[clientside {IP::local_addr}],ss_source_ip=[IP::local_addr],ss_dest_ip=[LB::server addr]"
+}
